404422d22e3cb6c59899a752be540141120bf82e
Ryftri <rayhanzulfitri@gmail.com>
2022-07-24 01:18:48 +0700

    menambahkan itur responsive

--- assets/JS/JavaScrip.js
+++ assets/JS/JavaScrip.js
@@ -1,4 +1,14 @@
 window.addEventListener("scroll", function () {
   var header = document.querySelector("header");
+  var navbarMenu = document.querySelector("ul");
   header.classList.toggle("sticky", window.scrollY > 200);
+  navbarMenu.classList.toggle("sticky", window.scrollY > 200);
+});
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector("ul");
+
+hamburger.addEventListener("click", function () {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
